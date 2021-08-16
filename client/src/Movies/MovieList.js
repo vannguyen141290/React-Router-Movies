6aeba177a707f00a87f1930d0cb155679c9cdb52
Sanoo Singh <vannguyen141290@gmail.com>
2021-08-16 14:55:47 -0700

    day1-project-completed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+
+          <MovieDetails key={movie.id} movie={movie} />
+
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { id, title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link to={`/movies/${id}`} style={{textDecoration: "none", color: "inherit"}}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
+
   );
 }
